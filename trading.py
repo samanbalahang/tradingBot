c489b9eb1eebea2d99297c102d3c89706163ee7f
samanbalahang <saman.balahang.2012@gmail.com>
2021-12-07 06:38:25 +0330

    add files

--- trading.py
+++ trading.py
@@ -7,10 +7,28 @@ from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.keys import Keys
 import webbrowser
 import time
+
 browser = webdriver.Chrome("chromedriver")
 browser.maximize_window()
+print("opening site please wait!...")
 browser.get("https://www.tradingview.com/symbols/18CBTC/technicals/")
-find_serial = browser.find_element_by_css_selector(".speedometerSignal-DPgs-R4s.sellColor-DPgs-R4sfirst-child")
-if find_serial.text == 'Sell': 
-    print("Sell")
-elseif find_serial.text == 'Sell':     
+time.sleep(10)
+while True:
+    print("tick")
+    print("opening site please wait!...")
+    find_serial = browser.find_element_by_css_selector("span.speedometerSignal-DPgs-R4s")
+    print("eleman is find you can have signal now")
+    print(find_serial.text)
+    time.sleep(10)
+    print("AFTERSLEEP: ")
+    if find_serial.text == 'SELL': 
+        print("Sell")
+    elif find_serial.text == 'STRONG SELL': 
+        print("Sell") 
+    elif find_serial.text == 'BUY': 
+        print("Buy")
+    elif find_serial.text == 'STRONG BUY': 
+        print("Buy") 
+    else:
+        print("nutral") 
+    time.sleep(300)
