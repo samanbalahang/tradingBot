02fef4e3a5d14de3fed35a82d8d48a10fe94c560
samanbalahang <saman.balahang.2012@gmail.com>
2021-12-13 08:55:26 +0330

    new edit

--- driver.py
+++ driver.py
@@ -6,57 +6,137 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.keys import Keys
 import webbrowser
+import datetime
 import time
 class driver:    
-    def getAnalyse(self,address, sleeptime,os):
-        print("opening site please wait!...")
+    def getAnalyse(self,address, sleeptime,os,child):
+        print(sleeptime)
+        child=child
         if(os == 1):
          browser = webdriver.Chrome("chromedriver.exe")
         else:
          browser = webdriver.Chrome("chromedriver")    
         browser.maximize_window()
         address = "https://www.tradingview.com/symbols/"+address+"USD/technicals/"
-        print(address)
+        print("opening "+ address+" site please wait!...")
+        print("browser should be complitly loaded be patient till then...")
         browser.get(address)
         time.sleep(10)
-        while True:
-            print("tick")
-            print("opening site please wait!...")
+        counter = 1
+        while True:            
+            print("OK!")
+            
             try:
-              find_serial = browser.find_element_by_css_selector(".tv-card-container__ideas.tv-card-container__ideas--with-padding")
+             #path = "#technicals-root .technicalsTab-DPgs-R4s .tabs-3-f9q69b"  
+              path = "#technicals-root .technicalsTab-DPgs-R4s .tabs-3-f9q69b button:"+child  
+              find_serial = browser.find_element_by_css_selector(path)
+              print("horray")
+             #print(path)
+              find_serial.click()
             except:
-                print("S")
+                print("")
             find_serial = browser.find_element_by_css_selector("span.speedometerSignal-DPgs-R4s")
-            print("eleman is find you can have signal now")
-            print(find_serial.text)
+            signal = find_serial.text
+            print("you can have signal after 10 secound")
             time.sleep(10)
-            print("AFTERSLEEP: ")
             if find_serial.text == 'SELL': 
-                print("Sell")
+                print("SELL")
             elif find_serial.text == 'STRONG SELL': 
-                print("Sell") 
+                print("STRONG SELL") 
             elif find_serial.text == 'BUY': 
-                print("Buy")
+                print("BUY")
             elif find_serial.text == 'STRONG BUY': 
-                print("Buy") 
+                print("STRONG BUY") 
             else:
                 print("nutral") 
-            time.sleep(sleeptime)
+            if(counter == 1):
+               print("inhere 1 !") 
+               time.sleep(sleeptime)
+            else: 
+               print("inhere!")  
+               try:
+                text = str(sleeptime) +" "
+                print(text)
+                text= text+str(datetime.datetime.now())+" "
+                print(text)
+                text = text+ str(signal)
+                print(text)
+               except:
+                print("notext") 
+               try:
+                   self.printTotxt(driver,text)  
+               except:
+                   print("")
+               sleeptime = int(sleeptime)         
+               time.sleep(sleeptime)
+            counter = counter + 1   
+    def printTotxt(self,text):
+        text_file = open("./data.txt", "a+")
+        text_file.write("\n")
+        text_file.write(text)
+                    
     def getdata(self,cripto,os,sleep):
+        if(sleep == '1'):
+            child = "first-child"
+            sleep = int(60)
+        elif(sleep == "2"): 
+            child = "nth-child(2)"
+            sleep = int(60*5)
+        elif(sleep == "3"): 
+            child = "nth-child(3)"
+            sleep = int(60*15)  
+        elif(sleep == "4"): 
+            child = "nth-child(4)"
+            sleep = int(60*30) 
+        elif(sleep == "5"): 
+            child = "nth-child(5)"
+            sleep = int(60*60*1)  
+        elif(sleep == "6"): 
+            child = "nth-child(6)"
+            sleep = int(60*60*2)  
+        elif(sleep == "7"): 
+            child = "nth-child(7)"
+            sleep = int(60*60*4)  
+        elif(sleep == "8"): 
+            child = "nth-child(8)"
+            sleep = int(60*60*12*1)  
+        elif(sleep == "9"): 
+            child = "nth-child(9)"
+            sleep = int(60*60*12*7)        
+        elif(sleep == "10"): 
+            child = "nth-child(10)"
+            sleep = int(60*60*12*30)     
         sleep = int(sleep)
-        if(cripto == '1'):
-            cripto = "BTC"
-            # print(cripto)
-        elif(cripto == "2"): 
-             cripto = "DOT" 
-            #  print(cripto)
-        elif(cripto == "3"):  
-            cripto = "LTC" 
-            # print(cripto)
-        elif(cripto == "4"):  
-            cripto = "ETH"  
-            # print(cripto)  
-        else:
-            print(cripto) 
-            # print("not validate")               
-        self.getAnalyse(driver,cripto,sleep,os)
+        print("we will check: "+cripto)          
+        self.getAnalyse(driver,cripto,sleep,os,child)
+    def printWhatIneed(self):
+        print("what is you'r operating system:")
+        print("please just put number")
+        print("1 : Windows")
+        print("2 : linux")
+        os = input("what is your os:\n")
+        print("what is you'r criptocurency:")
+        print("what is you'r criptocurency:")
+        print("type curency by its short code")
+        print("EXAMPLE: FOR GET SIGNAL FROM BITCOIN type:")
+        print("BTC")
+        cripto =  input("what is your criptocurency:\n")
+        print("i want report every ... secound")
+        print("1:   1  minute")
+        print("2:   5  minute")
+        print("3:   15 minute")
+        print("4:   30 minute")
+        print("5:   1 hour")
+        print("6:   2 hour")
+        print("7:   4 hour")
+        print("8:   1 day")
+        print("9:   1 week")
+        print("10:  1 month")
+        sleep = input("i whant my report every :\n")  
+        try:
+            self.getdata(driver,cripto,os,sleep)
+        except:
+            print("please just put number")  
+            print("1 : Windows")
+            print("2 : linux")
+            os = input("what is your os:\n")   
--- trading.py
+++ trading.py
@@ -1,26 +1,3 @@
-import time
 from driver import driver
-print("what is you'r operating system:")
-print("please just put number")
-print("1 : Windows")
-print("2 : linux")
-os = input("what is your os:\n")
-print("what is you'r criptocurency:")
-print("please just put number")
-print("1 : BTC Bitcoin ")
-print("2 : DOT BitcoinCash")
-print("3 : LTC Cardano")
-print("4 : ETH ETHEREUM")
-cripto =  input("what is your criptocurency:\n")
-print("i want report every ... secound")
-print("please just put number")
-print("for wxample input 10 for every 10 secound report")
-sleep = input("i whant my report every :\n")
-try:
-    driver.getdata(driver,cripto,os,sleep)
-except:
-    print("please just put number")  
-    print("1 : Windows")
-    print("2 : linux")
-    os = input("what is your os:\n") 
+driver.printWhatIneed(driver)
 
